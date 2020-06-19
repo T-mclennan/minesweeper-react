359a50074b7c021a611def8ff4be9ca9179d563e
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-06-19 11:06:39 -0700

    changed the favicon icon to a simple purple covid.png

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  apiGateway: {
+    REGION: 'us-west-1',
+    URL: 'https://91a4cw5h7k.execute-api.us-west-1.amazonaws.com/prod/',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './stylesheets/index.css';
 import App from './App';
+
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: 'scores',
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
